5902862ba33e1cbfb416e67c0d52b8f36b965bfa
NabilahSri <billb4721@gmail.com>
2024-08-18 20:07:09 +0700

    Revert "quiz"
    This reverts commit d24b2f0f9738e3934da6bf2b680f026803235347.

--- app/src/main/java/com/example/quizzinaja4/NumberModel.kt
+++
@@ -1,3 +0,0 @@
-package com.example.quizzinaja4
-
-data class NumberModel(var number: String)
--- app/src/main/java/com/example/quizzinaja4/QuizModel.kt
+++
@@ -1,4 +0,0 @@
-package com.example.quizzinaja4
-
-
-data class QuizModel(var id: String, var title: String, var time: String, var duration: String)
